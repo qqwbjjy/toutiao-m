93cca0b5c784500cb718977bf32e563fe78cb3c3
it <87961563@qq.com>
2023-09-11 15:46:13 +0800

    继续初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
+// 加载Vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 加载动态设置 REM 基准值
+import 'amfe-flexible'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
